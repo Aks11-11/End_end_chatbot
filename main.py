1a019de394e3a218c47b03da936b3a5fd8f5b34e
Aks11-11 <aksshatvashisht@gmail.com>
2024-04-20 16:07:56 +0530

    :)

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from sklearn.linear_model import LogisticRegression
 ssl._create_default_https_context = ssl._create_unverified_context
 nltk.data.path.append(os.path.abspath("nltk_data"))
 nltk.download('punkt')
-
 #Intent
 intents = [
     {
